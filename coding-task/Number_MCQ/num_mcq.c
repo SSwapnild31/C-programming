5325a41b7609f638b7ae45a2e5ad9e3941be83f7
Divate Swapnil <swapnil.nd7057@gmail.com>
2025-02-08 19:44:53 +0530

    04022025 tues

---
+++ coding-task/Number_MCQ/num_mcq.c
@@ -0,0 +1,24 @@
+//num mcq from 29 to 32
+
+#include<stdio.h>
+void main()
+{
+	/*char ch = 141;
+	printf("%d\n",ch);
+	*/				//-112
+	
+	/*char ch = 241;
+	printf("%d\n",ch);
+	*/				//-15
+	
+	/*char ch = 291;
+	printf("%c\n",ch);
+	*/				//#
+
+	char ch = 491;
+	printf("%d\n",ch);
+					//-21
+
+
+
+}
