e9769c9938077d6de0b93a431a0beb9de550d75b
Divate Swapnil <swapnil.nd7057@gmail.com>
2025-02-08 10:48:28 +0530

    Add files via upload

---
+++ function_data_passing/ex1.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+void main()
+{
+	int k=35;
+	printf("%d %d %d\n",k==35,k=50,k>40);
+
+}
---
+++ function_data_passing/ex2.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+	int i;
+	i = printf("Hello world...\n");
+	printf("i=%d\n",i);
+
+}
