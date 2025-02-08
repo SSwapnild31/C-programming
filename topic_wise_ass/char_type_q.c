c06bc8914fe12d600e0b107e32974284e9d200b1
Divate Swapnil <swapnil.nd7057@gmail.com>
2025-02-08 10:50:15 +0530

    Add files via upload

---
+++ topic_wise_ass/char_type_q.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+	//char ch = -200;		56
+	
+	//char ch = 128;		-128
+	
+	//unsigned char ch = -72;	184
+	
+	//printf("%d\n",ch);
+	
+	/*char ch = 48;
+	printf("%c\n",ch);		0
+	*/
+
+	char ch = 'abc';	
+	printf("%c\n",ch);		//c
+}
---
+++ topic_wise_ass/int_type_q.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main()
+{
+	/*int i = 353;
+        char ch = i;
+        printf("%c\n",ch);		//a
+	*/
+	
+	/*int i = 520;
+	short int ii = i;
+	printf("%hd\n",ii);
+	*/				//520
+
+	/*int i = 0101;
+	printf("%c\n",i);
+	*/				//A
+	
+	/*short int i =  1010;
+	char ch = i;
+	printf("%d\n",ch);
+	*/				//-14
+
+	int i = 0x101010;
+	printf("%o\n",i);		//4010020
+
+}
