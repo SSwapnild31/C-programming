a5b17abf9b7f509167f015873ec2a9dc8b3b6ebd
Divate Swapnil <swapnil.nd7057@gmail.com>
2025-02-08 19:42:59 +0530

    Add files via upload

---
+++ coding-task/char_mcq/char_mcq.c
@@ -0,0 +1,116 @@
+#include<stdio.h>
+void main()
+{
+	/*char mi = 100;
+	printf("%d\n",mi);
+	*/				//100
+	
+	/*char mi = 150;
+	printf("%d\n",mi);
+	*/				//-106
+	
+	/*char mi = 300;
+	printf("%d\n",mi);
+	*/				//44
+	
+	/*char mi = 400;
+	printf("%d\n",mi);
+	*/				//-112
+	
+	/*char mi = 510;
+	printf("%d\n",mi);
+	*/				//-2
+	
+	/*char mi = '2';
+	printf("%x\n",mi);
+	*/				//32
+	
+	/*char mi = 'E';
+	printf("%o\n",mi);
+	*/				//105
+
+	/*char mi = 'd';
+	printf("%x\n",mi);
+	*/				//64
+	
+	/*char mi = 260;
+	printf("%d\n",mi);
+	*/				//4
+	
+	/*char mi = '9';	
+	printf("%o\n",mi);
+	*/				//71
+	
+	/*char mi = 128;
+	printf("%d\n",mi);
+	*/				//-128
+		
+	/*char mi = 195;
+	printf("%d\n",mi);
+	*/				//-61
+	
+	/*char mi = 275;
+	printf("%d\n",mi);
+	*/				//19
+	
+	/*char mi = 365;
+	printf("%c\n",mi);
+	*/				//m
+	
+	/*char mi = 449;
+	printf("%d\n",mi);
+	*/				//-63
+	
+	/*unsigned char coding;
+	coding=502;
+	printf("%d\n",coding);
+	*/				//246
+	
+	/*unsigned char coding;
+	coding = 202;
+	printf("%d\n",coding);
+	*/				//202
+	
+	/*unsigned char coding;
+	coding = 311;
+	printf("%d\n",coding);
+	*/				//55
+	
+	/*unsigned char coding;
+	coding = 555;
+	printf("%d\n",coding);
+	*/				//43
+	
+	/*unsigned char coding;
+	coding = 'P';
+	printf("%d\n",coding);
+	*/				//80
+	
+	/*unsigned char sirji;
+	sirji =511;
+	printf("%d\n",sirji);
+	*/				//255
+	
+	/*unsigned char sirji;
+	sirji = 1010;
+	printf("%d\n",sirji);
+	*/				//242
+	
+	/*unsigned char sirji;
+	sirji = 1023;
+	printf("%d\n",sirji);
+	*/				//255
+	
+	unsigned char sirji;
+	sirji = -200;
+	printf("%d\n",sirji);
+					//56
+
+
+
+
+
+
+
+
+}
