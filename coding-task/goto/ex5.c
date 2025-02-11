4bc1541ca5d4e378c68799a7d3ca0aa51e8839fd
Divate Swapnil <swapnil.nd7057@gmail.com>
2025-02-11 23:05:49 +0530

    Update ex5.c
    updated logic , first reverse the given number and then do addition of their alternate digits

--- coding-task/goto/ex5.c
+++ coding-task/goto/ex5.c
@@ -1,16 +1,26 @@
 #include<stdio.h>
 void main()
 {
-	int num,r1,sum=0,i=0;
+	int num,r1,rev=0,r2,c=0,sum=0;
 	printf("Enter three or more digit number : \n");
 	scanf("%d",&num);
+	
+	//for reverse the original number 
       L1:	
 	r1 = num % 10;
-	if(i%2==0)
-		sum += r1;
 	num = num / 10;
-	i++;
+	rev = rev *10 +r1;
 	if(num>0)
 		goto L1;
-	printf("%d\n",sum);
+	
+	//sum of reversed numbers alternate digits
+      L2:
+	r2 = rev % 10;
+	rev = rev / 10;
+	if(c%2==0)
+		sum+=r2;
+	c++;
+	if(rev>0)
+		goto L2;
+	printf("sum of alternative num : %d\n",sum);
 }
