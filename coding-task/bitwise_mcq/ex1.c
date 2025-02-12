a3ce2250a8337d016bf9c471e9f74170f8d4cc70
Divate Swapnil <swapnil.nd7057@gmail.com>
2025-02-12 19:58:55 +0530

    12022025 wednesday

---
+++ coding-task/bitwise_mcq/ex1.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+	//printf("%d\n",-39>>3);	//-5
+	//printf("%d\n",-11^-11);	//0
+	//printf("%d\n",-'7'| 'C');	//-53
+	//printf("%d\n",-200 & -111);	//-240
+	
+	//printf("%d\n",'0' ^ -80);	//-128
+	//printf("%d\n",-10 << 4);	//-160
+	//printf("%x\n",-511 >>  10);	//ffffffff
+	//printf("%d\n",0x20 ^ -03);	//-35
+	
+	//printf("%o\n",0xBCA << 0xB);	//27450000
+	//printf("%d\n",'d' ^ -200);	//-164
+	//printf("%x\n",-1000);		//FFFFFC18
+	printf("%o\n",111+0x11);	//200
+	
+}
