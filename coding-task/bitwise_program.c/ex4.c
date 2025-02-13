e34fe3809255152306f899048cde5ff84e41e103
Divate Swapnil <swapnil.nd7057@gmail.com>
2025-02-13 20:08:41 +0530

    13022025 Thursday

---
+++ coding-task/bitwise_program.c/ex4.c
@@ -0,0 +1,16 @@
+//Write a Program in C to toggle 1st and 4th bit .
+//i/p: int n= 42 o/p: 56
+
+
+#include<stdio.h>
+void main()
+{
+	int num;
+	printf("Enter an number : ");
+	scanf("%d",&num);
+	
+	num=num^1<<1;
+	num=num^1<<4;
+	
+	printf("%d\n",num);
+}
