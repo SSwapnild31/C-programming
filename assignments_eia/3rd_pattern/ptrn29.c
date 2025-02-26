09cd2adb6d0048b3ffe78484d40b2ab69b5b16e5
Divate Swapnil <swapnil.nd7057@gmail.com>
2025-02-26 18:37:18 +0530

    26-02-2025 wed

---
+++ assignments_eia/3rd_pattern/ptrn29.c
@@ -0,0 +1,20 @@
+/*
+   *
+  ***
+ *****
+*******
+*/
+
+#include<stdio.h>
+void main()
+{
+	int i,j,k;
+	for(i=0;i<4;i++)
+	{
+		for(j=0;j<3-i;j++)
+			printf(" ");
+		for(k=0;k<=i*2;k++)
+			printf("*");
+		printf("\n");
+	}
+}
