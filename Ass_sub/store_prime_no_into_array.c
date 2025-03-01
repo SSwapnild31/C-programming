eabfdc4f2c9b7a14c8f4f17f3e26f1fbc004284f
Divate Swapnil <swapnil.nd7057@gmail.com>
2025-03-01 10:08:28 +0530

    Update store_prime_no_into_array.c

--- Ass_sub/store_prime_no_into_array.c
+++ Ass_sub/store_prime_no_into_array.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 void main()
 {
-	int ar[5],i,j;
+	int ar[5],i,j,ele;
+	ele=sizeof(a)/sizeof(a[0]);
 	printf("Enter array elements : \n");
-	for(i=0;i<5;)
+	for(i=0;i<ele;)
 	{
 		scanf("%d",&ar[i]);
 		for(j=2;j<ar[i];j++)
@@ -16,7 +17,7 @@ void main()
 	}
 	
 	printf("array elements are : ");
-	for(i=0;i<5;i++)
+	for(i=0;i<ele;i++)
 		printf("%d ",ar[i]);
 	printf("\n");
 }
