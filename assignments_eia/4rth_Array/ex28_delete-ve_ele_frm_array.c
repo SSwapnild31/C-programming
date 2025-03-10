365ca069a564939beb80dcba81c93ab5b2918013
Divate Swapnil <swapnil.nd7057@gmail.com>
2025-03-10 22:50:08 +0530

    10-03-2025 Mon

---
+++ assignments_eia/4rth_Array/ex28_delete-ve_ele_frm_array.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+void delete_neg(int [],int,int *);
+
+void main()
+{
+	int a[6],ele,i;
+	ele=6;
+	int nz;
+	printf("Enter array ele :\n");
+	for(i=0;i<ele;i++)
+		scanf("%d",&a[i]);
+	
+	delete_neg(a,ele,&nz);
+	
+	printf("after : ");
+	for(i=0;i<nz;i++)
+		printf("%d ",a[i]);
+	printf("\n");
+}
+
+void delete_neg(int a[],int ele,int *nz)
+{
+	int i,temp[ele],j=0;
+	
+	for(i=0;i<ele;i++)
+	{
+		if(a[i]>=0)
+			temp[j++]=a[i];
+	}
+	
+	for(i=0;i<j;i++)
+		a[i]=temp[i];
+	*nz=j;
+}
