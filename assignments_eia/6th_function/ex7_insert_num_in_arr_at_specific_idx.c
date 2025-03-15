18d704edf54fea4eecc3e6ee8f74c6c00772ce8c
Divate Swapnil <swapnil.nd7057@gmail.com>
2025-03-15 23:18:43 +0530

    15-03-2025 Saturday

---
+++ assignments_eia/6th_function/ex7_insert_num_in_arr_at_specific_idx.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+void print(int *,int);
+void insert_arr(int *,int,int,int);
+void main()
+{
+	int a[6]={1,2,3,4,5},ele,i,num,idx;
+	ele=sizeof(a)/sizeof(a[0]);
+	
+	/*printf("Enter array ele :\n");
+	for(i=0;i<ele-1;i++)
+		scanf("%d",a+i);
+	*/
+	printf("Enter num: ");
+	scanf("%d",&num);
+	printf("Enter index : ");
+	scanf("%d",&idx);
+	
+	print(a,ele-1);
+	insert_arr(a,ele,num,idx);
+	print(a,ele);
+}
+void insert_arr(int *a,int ele,int num,int idx)
+{
+	int i;
+	for(i=ele;i>idx;i--)
+	{
+		a[i]=a[i-1];
+		printf("a[i] : %d = a[i-1] : %d\n",a[i],a[i-1]);
+	}
+	a[idx]=num;
+	printf("a[i] : %d i : %d\n",a[idx],i);
+}
+
+void print(int *p,int ele)
+{
+	for(int i=0;i<ele;i++)
+		printf("%d ",p[i]);
+	printf("\n");
+}
