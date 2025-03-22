9e74b0aeca1c7117245a4619d15bc1d59cb40299
Divate Swapnil <swapnil.nd7057@gmail.com>
2025-03-22 11:52:56 +0530

    22-03-2025 Sat

---
+++ assignments_eia/5th_String/ex32_remove_conjuctive_spaces_frm_str.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+void main()
+{
+	char s[30];
+	printf("Enter string : ");
+	scanf("%[^\n]",s);
+	
+	int i=0;
+	while(s[i])
+	{
+		if(s[i]==' '&& s[i+1]==' ')
+		{
+			for(int j=i+1;s[j];j++)
+				s[j-1]=s[j];
+			i--;
+		}
+		i++;
+	}
+	
+	printf("%s\n",s);
+}
