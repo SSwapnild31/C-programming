824f83c036ce9a1bc34f587a1f8bc329ac9ff72c
Divate Swapnil <swapnil.nd7057@gmail.com>
2025-03-23 14:16:14 +0530

    Update ex31_count_all_duplicate_ele_frm_arr.c

--- assignments_eia/4rth_Array/ex31_count_all_duplicate_ele_frm_arr.c
+++ assignments_eia/4rth_Array/ex31_count_all_duplicate_ele_frm_arr.c
@@ -16,20 +16,23 @@ void main()
 
 void count_dupli(const int *p,int ele)
 {
-	int i,j,c;
-	for(i=0;i<ele;i++)
-	{
-		for(j=i+1,c=1;j<ele;j++)
-		{
-			if(p[i]==p[j])
-				c++;
-		}
-		if(c>1)
-			printf("%d -> %d times, ",p[i],c);
-		c=0;
-	}	
-	printf("\n");
+        int i,j,k,c;
+        for(i=0;i<ele;i++)
+        {
+                for(j=i+1,k=i-1,c=1;j<ele;j++,k--)
+                {
+                        if(p[i]==p[k] && i!=0)
+                                break;
+                        else if(p[i]==p[j])
+                                c++;
+                }
+                if(c>1)
+                        printf("%d -> %d times, ",p[i],c);
+                c=0;
+        }
+        printf("\n");
 }
+
 
 void print(const int *p,int ele)
 {
