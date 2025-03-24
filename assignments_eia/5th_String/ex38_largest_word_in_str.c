decea4e5a1e81575dcedd513b3f0b70072da12f9
Divate Swapnil <swapnil.nd7057@gmail.com>
2025-03-24 16:13:42 +0530

    Done ex38_largest_word_in_str.c

--- assignments_eia/5th_String/ex38_largest_word_in_str.c
+++ assignments_eia/5th_String/ex38_largest_word_in_str.c
@@ -1,52 +1,51 @@
 #include<stdio.h>
-void my_strcpy(char *,char *);
+void my_strncpy(char *,char *,int);
 int my_strlen(const char *);
 char * my_strchr(char *,char);
 void main()
 {
-	char s[30];
+	char s[50];
 	printf("Enter string : ");
 	scanf("%[^\n]",s);
 	
-	char *p=s,*q,lw[10];
+	char *p=s,*q,lw[25];
 	int len=0,len1;
 	
 	while(q=my_strchr(p,' '))
 	{	
-		len1=my_strlen(p);
+		len1=q-p;
 		if(len1>len)
 		{
 			len=len1;
-			my_strcpy(lw,p);
+			my_strncpy(lw,p,len1);
 		}
 		p=q+1;
 	}
-	q=my_strchr(p,'\0');
 	len1=my_strlen(p);
 	if(len1>len)
 	{
 		len=len1;
-		my_strcpy(lw,p);
+		my_strncpy(lw,p,len1);
 	}
 	printf("%s\n",lw);
 	
 }
 
-void my_strcpy(char *d,char *s)
+void my_strncpy(char *d,char *s,int len1)
 {
 	int i;
-	for(i=0;s[i]!=' ';i++)
+	for(i=0;i<len1;i++)
 		d[i]=s[i];
 	d[i]='\0';
 }
 
 char * my_strchr(char *p,char ch)
 {
-	
-	for(int i=0;p[i];i++)
+	while(*p)
 	{
-		if(p[i]==ch)
-			return &p[i];
+		if(*p==ch)
+			return p;
+		p++;
 	}
 	return 0;
 }
@@ -54,6 +53,6 @@ char * my_strchr(char *p,char ch)
 int my_strlen(const char *p)
 {
 	int i;
-	for(i=0;p[i]!=' ';i++);
+	for(i=0;p[i];i++);
 	return i;	
 }
