c3fb8e692bd5668b83658d6973489913816d10de
Divate Swapnil <swapnil.nd7057@gmail.com>
2025-03-26 11:29:56 +0530

    26-03-2025 wedn

--- assignments_eia/5th_String/ex41_check_two_strings_are_anagram_or_not.c
+++ assignments_eia/5th_String/ex41_check_two_strings_are_anagram_or_not.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
-
-int lenstr(char *);
+#include<string.h>
+void strshort(char *,int);
 void copy(char *,char *);
 int anagram(char *,char *);
 void main()
@@ -13,10 +13,13 @@ void main()
 	
 	copy(m1,s1);
 	copy(m2,s2);
-	
-	int len1=lenstr(m1);
-	int len2=lenstr(m2);
 
+	int len1=strlen(m1);
+	int len2=strlen(m2);
+	
+	strshort(m1,len1);
+	strshort(m2,len2);
+	
 	if(len1!=len2)
 	{
 		printf("not anagram..!");
@@ -29,14 +32,29 @@ void main()
 		printf("not anagram..!\n");
 }
 
+void strshort(char *m,int len)
+{
+	for(int i=0;i<len-1;i++)
+	{
+		for(int j=i+1;j<len;j++)
+		{
+			if(m[i]>m[j])
+			{
+				char t=m[i];
+				m[i]=m[j];
+				m[j]=t;
+			}
+		}
+	}
+	printf("%s\n",m);
+}
+
 int anagram(char *m1,char *m2)
 {
 	for(int i=0;m1[i];i++)
 	{
-		for(int j=0;m2[j];j++)
-		{
-			if(m1[i]==m2[j])
-		}
+		if(m1[i]!=m2[i])
+			return 0;
 	}
 	return 1;
 }
