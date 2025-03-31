534d262d2b7425442e99c8218a4466a2fd4119e8
Divate Swapnil <swapnil.nd7057@gmail.com>
2025-03-31 19:18:36 +0530

    31-0-2025 monday

---
+++ arr_of_pointers/ex1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void main()
+{
+	int i=10,j=20,k=30;
+	int *p[3];
+	
+	p[0]=&i;
+	p[1]=&j;
+	p[2]=&k;
+	
+	
+	printf("%d %d %d\n",*p[0],*p[1],*p[2]);
+	printf("%d %d %d\n",p[0][0],p[1][0],p[2][0]);
+}
