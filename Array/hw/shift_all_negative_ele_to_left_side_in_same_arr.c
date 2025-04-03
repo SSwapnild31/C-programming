2d3047d115edec8a186befbf5dda44d2a0b9006a
Divate Swapnil <swapnil.nd7057@gmail.com>
2025-04-03 17:37:53 +0530

    03-04-2025 Thurs

--- Array/hw/shift_all_negative_ele_to_left_side_in_same_arr.c
+++ Array/hw/shift_all_negative_ele_to_left_side_in_same_arr.c
@@ -8,8 +8,20 @@ void main()
 	for(i=0;i<ele;i++)
 		scanf("%d",a+i);
 	
+	int j,k=0,num;
 	for(i=0;i<ele;i++)
 	{
-		
+		if(a[i]==0)
+		{
+			num=a[i];
+			for(j=i;j>k;j--)
+				a[j]=a[j-1];
+			a[k++]=num;
+		}
 	}
+	
+	printf("after : ");
+	for(i=0;i<ele;i++)
+		printf("%d ",a[i]);
+	printf("\n");
 }
