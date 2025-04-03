09f619172d56d07f1643de684c8ee5d03bdc6759
Divate Swapnil <swapnil.nd7057@gmail.com>
2025-04-03 23:01:51 +0530

    03-04-2025 Thu

--- assignments_eia/5th_String/ex34_count_duplicate_char_frm_str.c
+++ assignments_eia/5th_String/ex34_count_duplicate_char_frm_str.c
@@ -6,15 +6,22 @@ void main()
 	printf("Enter string : ");
 	scanf("%[^\n]",s);
 	
-	int i,j,c;
+	int i,j,c=0,flag=0;
 	for(i=0;s[i];i++)
 	{
-		for(j=i+1,k=i-1;s[j];j++,k--)
+		c=1;
+		flag=0;
+		for(j=0;j<i;j++)
+			if(s[i]==s[j])
+				flag=1;
+		if(flag)
+			continue;
+		for(int k=i+1;s[k];k++)
 		{
-			if(s[i]==s[j] )
-				
-			
+			if(s[i]==s[k] )
+				c++;		
 		}
-		printf("%d\n",c);
+		if(c>1)
+			printf("%c-->%d\n",s[i],c);
 	}
 }
