2e819714ce8ad96472d994d6d5d53bcdbf881dc3
Divate Swapnil <swapnil.nd7057@gmail.com>
2025-04-13 23:52:25 +0530

    13-04-2025 Sun

---
+++ assignments_eia/7th_recursion/ex7_count_set_bit_frm_give_num.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int rec_fun_bitCount(int);
+int bitCount_helper(int,int);
+void main()
+{
+	int num;
+	printf("Enter num : ");
+	scanf("%d",&num);
+	
+	int count=rec_fun_bitCount(num);
+	printf("set bit count : %d\n",count);
+}
+
+int rec_fun_bitCount(int num)
+{
+	return bitCount_helper(num,31);
+}
+
+int bitCount_helper(int num,int pos)
+{
+	if(pos<0)
+		return 0;
+	if(num<<pos)
+		return 1+bitCount_helper(num,pos-1);
+	return bitCount_helper(num,pos-1);
+}
