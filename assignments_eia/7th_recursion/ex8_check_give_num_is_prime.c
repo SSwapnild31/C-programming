c9842450f0174d1e3948980468350ff84360228a
Divate Swapnil <swapnil.nd7057@gmail.com>
2025-04-14 00:17:13 +0530

    13-04-2025 Sun

--- assignments_eia/7th_recursion/ex8_check_give_num_is_prime.c
+++ assignments_eia/7th_recursion/ex8_check_give_num_is_prime.c
@@ -1,7 +1,25 @@
 #include<stdio.h>
 
+int rec_fun_prime(int,int);
 
 void main()
 {
+	int num;
+	printf("Enter num : ");
+	scanf("%d",&num);
+	
+	int prime=rec_fun_prime(num,2);
+	if(prime)
+		printf("%d is not prime\n",num);
+	else
+		printf("%d is prime\n",num);
+}
 
+int rec_fun_prime(int num,int i)
+{
+	if(i>=num)
+		return 0;
+	if(num%i==0)
+		return 1;
+	return rec_fun_prime(num,i+1);
 }
