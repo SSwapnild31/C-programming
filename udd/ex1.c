6557ba950e7e9f058604feedad7d383d35ae5ab2
Divate Swapnil <swapnil.nd7057@gmail.com>
2025-04-21 23:22:55 +0530

    21-04-2025 Mon

--- udd/ex1.c
+++ udd/ex1.c
@@ -1,18 +1,10 @@
 #include<stdio.h>
-struct one
-{
-	int i;
-	char ch;
-	float f;	
-};
+
+typedef unsigned char u8;
 
 void main()
 {
-	struct one v;
-	
-	v.i=10;
-	v.ch='a';
-	v.f=34.5;
-	
-	printf("%d %c %f\n",v.i,v.ch,v.f);
+	u8 ch;
+	ch='A';
+	printf("%c\n",ch);
 }
