9b6c5f321bef4a397fa998a08af7a77901591421
Divate Swapnil <swapnil.nd7057@gmail.com>
2025-04-27 21:58:59 +0530

    27-04-2025 Sun

---
+++ udd/struct/hw/dma_n_student_rollno_even.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct st
+{
+	int rollno;
+	char name[10];
+	float marks;
+};
+
+void main()
+{
+	int n,i;
+	printf("Enter size of students : ");
+	scanf("%d",&n);
+	
+	struct st **p;
+
+	p=malloc(sizeof(struct st *)*n);
+	
+	for(i=0;i<n;i++)
+		p[i]=malloc(sizeof(struct st)*n);
+	
+	printf("Enter student rollno, name & marks\n");
+	for(i=0;i<n;i++)
+		scanf("%d %s %f",&p[i]->rollno,p[i]->name,&p[i]->marks);
+
+	printf("student data\n");
+	for(i=0;i<n;i++)
+	{
+		if(p[i]->rollno%2==0)
+			printf("%d %s %f\n",p[i]->rollno,p[i]->name,p[i]->marks);
+	}
+
+	free(p);
+	p=0;
+}
