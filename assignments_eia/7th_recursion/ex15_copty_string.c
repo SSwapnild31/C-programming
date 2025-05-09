dbb49d99e08e6a8c406a1a853e7db1b23fbd977b
Divate Swapnil <swapnil.nd7057@gmail.com>
2025-05-09 23:09:55 +0530

    09-05-2025 Fri

--- assignments_eia/7th_recursion/ex15_copty_string.c
+++ assignments_eia/7th_recursion/ex15_copty_string.c
@@ -1,28 +1,33 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-void my_strcpy(char *,char *);
+void my_strncpy(char *,char *,int);
 void main()
 {
 	char *s,*d;
+	int n;
 	
 	s=malloc(30);
 	d=malloc(30);
 	
 	printf("Enter string : ");
 	scanf("%[^\n]",s);
-
+	printf("Enter n : ");
+	scanf("%d",&n);
+	
 	printf("before d : %s\n",d);
 	
-	my_strcpy(d,s);
+	my_strncpy(d,s,n);
 	
 	printf("after d : %s\n",d);
 }
 
-void my_strcpy(char *d, char *s)
+void my_strncpy(char *d, char *s,int n)
 {
-	if(!*s)
+	static int c;
+	if(c==n)
 		return ;
 	*d++=*s++;
-	my_strcpy(d,s);
+	c++;
+	my_strncpy(d,s,n);
 }
