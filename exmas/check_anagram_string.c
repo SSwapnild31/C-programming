f20e8458cee4fed341f9961a69dea9a111ae4e24
Swapnil Divate <swapnil.nd7057@gmail.com>
2025-09-02 23:18:57 +0530

    02-09-2025 Tue

---
+++ exmas/check_anagram_string.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{	
+	char s1[20],s1[20],p1[20],p[20];
+	
+	printf("Enter 1st string : ");
+	scanf("%s",s1);
+	
+	printf("Enter 2nd string : ");
+	scanf("%s",s2);
+	
+	
+	
+	return 0;
+}
