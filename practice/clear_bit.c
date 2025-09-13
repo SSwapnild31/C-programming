3a4ee466cf362792a65fcb74ceceef528939d1d8
Swapnil Divate <swapnil.nd7057@gmail.com>
2025-09-13 22:36:58 +0530

    13-09-2025 sat

---
+++ practice/clear_bit.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+{
+	int num,pos;
+
+	printf("Enter number : ");
+	scanf("%d",&num);
+	printf("Enter pos : ");
+	scanf("%d",&pos);
+	
+	for(int i=31;i>=0;i--)
+		printf("%d",num>>i&1);
+	printf("\n");
+	
+	num = num&~(1<<pos);
+
+	for(int i=31;i>=0;i--)
+		printf("%d",num>>i&1);
+	printf("\n");
+	
+	return 0;
+}
